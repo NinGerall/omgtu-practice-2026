985b5c7a4f4e5f8efdd799aa7b6f005959ce20c0
NinGerall <135425135+NinGerall@users.noreply.github.com>
2026-07-04 00:26:59 +0600

    Выполнено задание 4: Шаги 3-5 (Интерфейс ISpaceship, классы Cruiser и Fighter, xUnit тесты)

---
+++ task04/task04/Cruiser.cs
@@ -0,0 +1,12 @@
+namespace task04
+{
+    public class Cruiser : ISpaceship
+    {
+        public int Speed => 50;
+        public int FirePower => 100;
+
+        public void MoveForward() { }
+        public void Rotate(int angle) { }
+        public void Fire() { }
+    }
+}
